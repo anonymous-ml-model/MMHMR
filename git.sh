2d9975d488102c7cbac4646fa5162a7185dc9d80
anonymous-ml-model <usama.saleem7977@gmail.com>
2024-11-13 16:24:23 -0500

    template

--- git.sh
+++ git.sh
@@ -1,6 +1,7 @@
-git config user.name "anonymous-ml-model"
-git config user.email "usama.saleem7977@gmail.com"
+
 git remote set-url origin https://anonymous-ml-model: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@github.com/anonymous-ml-model/MMHMR.git
+git reset --soft HEAD~1  # Undo the last commit while keeping changes staged
+
 git add .
 git commit -m "template"
 git push origin main
