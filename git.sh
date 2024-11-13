bf20707597782da3267bd8988629bcd08f7f4086
anonymous-ml-model <usama.saleem7977@gmail.com>
2024-11-13 15:26:45 -0500

    template

--- git.sh
+++ git.sh
@@ -1,5 +1,6 @@
 git config user.name "anonymous-ml-model"
 git config user.email "usama.saleem7977@gmail.com"
+git remote set-url origin https://anonymous-ml-model@github.com/anonymous-ml-model/MMHMR.git
 git add .
 git commit -m "template"
 git push
